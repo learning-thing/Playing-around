d4891fe8bc2e98b5632b78f1bea67432bb323827
Tinolm <OMG-ichfind-keinen-namen@protonmail.com>
2023-10-29 20:14:19 +0100

    Styling

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ var stuff = ["Bruh", "lol", "Yoo", "Amogus", "SUS", "David0IV0"]
 var DavidSpotify = "https://spotify.link/of1XE4A2UCb";
 
 
+document.getElementById("mainvid").volume = 0.1;
 
-for (let i=0; i<10; i++) {
-    colors.push('#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6));
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+for (let i=0; i<stuff.length*2  ; i++) {
+    colors.push('#'+(0x1000000+Math.random()*0xffffff).toString(16).substr(1,6));
+}
 
 for (let i = 0; i<colors.length; i++) {
     var a = document.createElement('a');
     var spam = document.createElement("span");
     var rand = Math.round(-1+Math.random()*10);
     console.log(linkText);
-
     a.appendChild(spam)
 
     while (rand > stuff.length-1 || rand < 0) {
         rand = Math.round(-1+Math.random()*10);
     }
-
 
     var linkText = document.createTextNode(stuff[rand]);
     console.log(linkText);
@@ -37,4 +39,5 @@ for (let i = 0; i<colors.length; i++) {
     }
 
     document.body.appendChild(a);
+    sleep(5000);
  }
